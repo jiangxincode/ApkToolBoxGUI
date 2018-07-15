14f6b29295e32af6869f82a035fec856fd712824
jiangxincode <jiangxinnju@163.com>
2018-07-15 12:19:58 +0800

    update

--- src/main/java/edu/jiangxin/i18n/tools/I18NUtils.java
+++ src/main/java/edu/jiangxin/i18n/tools/I18NUtils.java
@@ -14,6 +14,7 @@ import org.jdom2.output.XMLOutputter;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
@@ -21,9 +22,10 @@ import java.util.Map;
 public class I18NUtils {
 	private static final String sourceBaseStr = "D:\\temp\\Java\\I18NTools\\src\\test\\resources\\res1";
 
-	private static final String targetBaseStr = "D:\\temp\\Java\\I18NTools\\src\\test\\resources\\res3";
+	private static final String[] targetBaseStrs = new String[] {
+			"D:\\temp\\Java\\I18NTools\\src\\test\\resources\\res3" };
 
-	private static final String itemName = "storage_is_full";
+	private static final String[] itemNames = new String[] { "storage_is_full" };
 
 	private static final String charset = "UTF-8";
 
@@ -33,8 +35,24 @@ public class I18NUtils {
 
 	private static final boolean isRemoveLastLF = true;
 
+	private static Map<String, String> replace = new HashedMap<String, String>();
+
+	static {
+		replace.put("&quot;", "jiangxin001");
+		replace.put("&#160;", "jiangxin002");
+	}
+
 	public static void main(String[] args) throws JDOMException, IOException {
 
+		for (String targetBaseStr : targetBaseStrs) {
+			for (String itemName : itemNames) {
+				innerProcessor(targetBaseStr, itemName);
+			}
+		}
+	}
+
+	private static void innerProcessor(String targetBaseStr, String itemName)
+			throws JDOMException, IOException, FileNotFoundException {
 		File sourceBaseFile = new File(sourceBaseStr);
 		File targetBaseFile = new File(targetBaseStr);
 
@@ -47,12 +65,6 @@ public class I18NUtils {
 		for (File sourceParentFile : sourceParentFiles) {
 			File sourceFile = new File(sourceParentFile, "strings.xml");
 			if (sourceFile.exists()) {
-
-				Map<String, String> replace = new HashedMap<String, String>();
-
-				replace.put("&quot;", "jiangxin001");
-				replace.put("&#160;", "jiangxin002");
-
 				SAXBuilder builder = new SAXBuilder();
 				Document sourceDoc = builder.build(sourceFile);
 				Element sourceRoot = sourceDoc.getRootElement();
@@ -65,7 +77,7 @@ public class I18NUtils {
 						logger.info("count: " + (++count) + ", in path: " + sourceFile.getCanonicalPath()
 								+ ", out path: " + targetFile.getCanonicalPath());
 						if (targetFile.exists()) {
-							prePocess(targetFile, replace);
+							prePocess(targetFile);
 							Document targetDoc = builder.build(targetFile);
 							Element targetRoot = targetDoc.getRootElement();
 							boolean isFinished = false;
@@ -93,7 +105,7 @@ public class I18NUtils {
 							format.setLineSeparator("\n");
 							out.setFormat(format);
 							out.output(targetDoc, new FileOutputStream(targetFile));
-							postProcess(targetFile, replace);
+							postProcess(targetFile);
 						} else {
 							logger.error("output file doesn/t exist.");
 						}
@@ -104,17 +116,17 @@ public class I18NUtils {
 		}
 	}
 
-	private static void prePocess(File file, Map<String, String> map) throws IOException {
+	private static void prePocess(File file) throws IOException {
 		String content = FileUtils.readFileToString(file, charset);
-		for (Map.Entry<String, String> entry : map.entrySet()) {
+		for (Map.Entry<String, String> entry : replace.entrySet()) {
 			content = content.replaceAll(entry.getKey(), entry.getValue());
 		}
 		FileUtils.writeStringToFile(file, content, charset);
 	}
 
-	private static void postProcess(File file, Map<String, String> map) throws IOException {
+	private static void postProcess(File file) throws IOException {
 		String content = FileUtils.readFileToString(file, charset);
-		for (Map.Entry<String, String> entry : map.entrySet()) {
+		for (Map.Entry<String, String> entry : replace.entrySet()) {
 			content = content.replaceAll(entry.getValue(), entry.getKey());
 		}
 		if (isRemoveLastLF) {
