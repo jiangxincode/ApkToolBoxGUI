c71aeb8f179553db70513c33872bbb5911516312
Aloys <jiangxinnju@163.com>
2021-12-19 09:19:33 +0800

    fix #53: adaptive zoom factor

--- src/main/java/edu/jiangxin/apktoolbox/convert/color/ColorPickerPanel.java
+++ src/main/java/edu/jiangxin/apktoolbox/convert/color/ColorPickerPanel.java
@@ -2,6 +2,7 @@ package edu.jiangxin.apktoolbox.convert.color;
 
 import edu.jiangxin.apktoolbox.swing.extend.EasyPanel;
 import edu.jiangxin.apktoolbox.utils.Constants;
+import org.apache.commons.lang3.StringUtils;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -34,58 +35,42 @@ public class ColorPickerPanel extends EasyPanel {
 
     private static final int RIGHT_PADDING = 10;
 
-    private static final int COLOR_RECT_HEIGHT = 60;
-
-    private static final int COORDINATE_HEIGHT = 20;
-
-    private static final int COLOR_LABEL_HEIGHT = 20;
-
-    private static final int COMBO_HEIGHT = 20;
-
-    private static final int ZOOM_RECT_HEIGHT = 100;
-
-    private static final int SHOW_FIELD_HEIGHT = 20;
-
-    private static final int COLOR_RECORD_ITEM_HEIGHT = 20;
-
-    private static final int RECORD_RECT_HEIGHT = 100;
-
-    private static final int COPY_BUTTON_HEIGHT = 20;
-
-    private static final int TEXT_AREA_HEIGHT = 100;
-
     private static final int TEXT_AREA_WIDTH = 200;
+
+    private static final int DEFAULT_HEIGHT_SMALL = 20;
+
+    private static final int DEFAULT_HEIGHT_BIG = 100;
 
     private static final int DEFAULT_WIDTH = 100;
 
     private static final int WIDTH = LEFT_PADDING + DEFAULT_WIDTH * 3 + Constants.DEFAULT_X_BORDER * 3 + TEXT_AREA_WIDTH + RIGHT_PADDING;
 
-    private static final int HEIGHT = TOP_PADDING + COLOR_RECT_HEIGHT + COORDINATE_HEIGHT + COLOR_LABEL_HEIGHT + Constants.DEFAULT_Y_BORDER + COMBO_HEIGHT + BOTTOM_PADDING;
+    private static final int HEIGHT = TOP_PADDING + DEFAULT_HEIGHT_SMALL * 3 + DEFAULT_HEIGHT_BIG + Constants.DEFAULT_Y_BORDER * 2 + BOTTOM_PADDING;
+
+    private JPanel colorPanel;
+    private JLabel coordinateLabel;
+    private JLabel colorLabel;
 
     private Robot robot;
-
-    private JPanel colorPanel; // 颜色展示面板
-    private JLabel coordsLabel; // 坐标信息
-    private JLabel colorLabel; // 颜色信息
     private Point mousePoint; // 光标点
     private Image areaImage; // 待放大的图片
-    private final int zoomValue = 3; // 放大倍数，（只会放大到100像素）
-    private static final int ZoomMax = 100; // 总的放大倍数，不能更改
-    private final int sideLength = ZoomMax / zoomValue;
-    // 上一次的光标位置
-    private Point prevPoint = null;
-    // 当前的颜色，按键记录才记录163, 184, 204
-    private Color currentColor;
-    // 当前颜色模式
-    private static ColorMode currentColorMode = ColorMode.RGB;
-    // 交叉线
-    private Line2D crossHorizontal;
+
+    private int zoomFactor = 2;
+
+    private Point prevPoint = null; // 上一次的光标位置
+
+    private Color currentColor; // 当前的颜色，按键记录才记录163, 184, 204
+
+    private static ColorMode currentColorMode = ColorMode.RGB; // 当前颜色模式
+
+    private Line2D crossHorizontal; // 交叉线
     private Line2D crossVertical;
-    // 记录颜色历史记录, 用LinkedList队列，
+
     private int colorRecordMax = 5; // 记录的color record个数
-    private LinkedList<Color> colorQueue = new LinkedList<Color>();
-    // 颜色值展示框
-    JTextField showField;
+    private LinkedList<Color> colorQueue = new LinkedList<>();
+
+    private JTextField colorCopyTextField;
+    private JButton colorCopyButton;
 
     // 边框
     private final Rectangle2D colorRect = new Rectangle2D.Double();
@@ -104,84 +89,16 @@ public class ColorPickerPanel extends EasyPanel {
     }
 
     private void initUI() {
-        // 无布局方式
         setLayout(null);
 
-        // 左侧的颜色框和显示的坐标，颜色值
-        // 添加 panel 组件用来做颜色框
-        colorPanel = new JPanel();
-        colorPanel.setBounds(LEFT_PADDING, TOP_PADDING, DEFAULT_WIDTH, COLOR_RECT_HEIGHT);
-        add(colorPanel);
-        // 坐标
-        coordsLabel = new JLabel();
-        coordsLabel.setBounds(LEFT_PADDING, TOP_PADDING + COLOR_RECT_HEIGHT, DEFAULT_WIDTH, COORDINATE_HEIGHT);
-        add(coordsLabel);
-        // 颜色值
-        colorLabel = new JLabel();
-        colorLabel.setBounds(LEFT_PADDING, TOP_PADDING + COLOR_RECT_HEIGHT + COORDINATE_HEIGHT, DEFAULT_WIDTH, COLOR_LABEL_HEIGHT);
-        add(colorLabel);
+        initFirstColumn();
 
-        // 放大镜的十字线（大小100,100）
-        crossHorizontal = new Line2D.Double(
-                (double) LEFT_PADDING + DEFAULT_WIDTH + Constants.DEFAULT_X_BORDER,
-                (double) TOP_PADDING + ZOOM_RECT_HEIGHT / 2.0,
-                (double) LEFT_PADDING + DEFAULT_WIDTH * 2 + Constants.DEFAULT_X_BORDER,
-                (double) TOP_PADDING + ZOOM_RECT_HEIGHT / 2.0);
-        crossVertical = new Line2D.Double(
-                (double) LEFT_PADDING + DEFAULT_WIDTH + Constants.DEFAULT_X_BORDER + DEFAULT_WIDTH / 2.0,
-                TOP_PADDING,
-                (double) LEFT_PADDING + DEFAULT_WIDTH + Constants.DEFAULT_X_BORDER + DEFAULT_WIDTH / 2.0,
-                (double) TOP_PADDING + ZOOM_RECT_HEIGHT);
+        initSecondColumn();
 
-        // 右侧的颜色背景和颜色值label。大小100, 100
-        for (int i = 0; i < colorRecordMax; i++) {
-            colorRecordValue[i] = new JLabel();
-            colorRecordValue[i].setOpaque(true); // 背景不透明
-            colorRecordValue[i].setBounds(LEFT_PADDING + DEFAULT_WIDTH * 2 + Constants.DEFAULT_X_BORDER * 2,
-                    TOP_PADDING + i * COLOR_RECORD_ITEM_HEIGHT, DEFAULT_WIDTH, COLOR_RECORD_ITEM_HEIGHT);
-            add(colorRecordValue[i]);
-        }
+        initThirdColumn();
 
-        // 颜色模式选择框
-        JComboBox<String> colorModeCombo = new JComboBox<>();
-        for (ColorMode t : ColorMode.values()) {
-            colorModeCombo.addItem(t.toString());
-        }
-        // 给选择框绑定一个事件，改变时触发，用来设置当前颜色模式
-        colorModeCombo.addActionListener(event -> {
-            currentColorMode = ColorMode.valueOf((String) colorModeCombo.getSelectedItem());
-        });
-        colorModeCombo.setBounds(LEFT_PADDING, TOP_PADDING + COLOR_RECT_HEIGHT + COORDINATE_HEIGHT + COLOR_LABEL_HEIGHT + Constants.DEFAULT_Y_BORDER, DEFAULT_WIDTH, COMBO_HEIGHT);
-        add(colorModeCombo);
+        initFourthColumn();
 
-        // 颜色值框体
-        showField = new JTextField();
-        showField.setEditable(false);
-        showField.setBounds(LEFT_PADDING + DEFAULT_WIDTH + Constants.DEFAULT_X_BORDER, TOP_PADDING + ZOOM_RECT_HEIGHT + Constants.DEFAULT_Y_BORDER, DEFAULT_WIDTH, SHOW_FIELD_HEIGHT);
-        add(showField);
-
-        // 复制按钮
-        JButton copyButton = new JButton("Copy");
-        copyButton.addActionListener(event -> {
-            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-            // 封装文本内容
-            Transferable trans = new StringSelection(getColorText(currentColor));
-            // 把文本内容设置到系统剪贴板
-            clipboard.setContents(trans, null);
-
-        });
-        copyButton.setBounds(LEFT_PADDING + DEFAULT_WIDTH * 2 + Constants.DEFAULT_X_BORDER * 2, TOP_PADDING + RECORD_RECT_HEIGHT + Constants.DEFAULT_Y_BORDER, DEFAULT_WIDTH, COPY_BUTTON_HEIGHT);
-        add(copyButton);
-
-        JTextArea textArea = new JTextArea();
-        textArea.setBackground(Color.PINK);
-        textArea.setBounds(LEFT_PADDING + DEFAULT_WIDTH * 3 + Constants.DEFAULT_X_BORDER * 3, TOP_PADDING, TEXT_AREA_WIDTH, TEXT_AREA_HEIGHT);
-        textArea.append("记录颜色: ALT+C" + System.getProperty("line.separator"));
-        textArea.append("锁定区域: ALT+L" + System.getProperty("line.separator"));
-        textArea.append("取消锁定区域: ALT+U" + System.getProperty("line.separator"));
-        add(textArea);
-
-        // 键盘检测事件
         final InputMap inputMap = colorPanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
         inputMap.put(KeyStroke.getKeyStroke("alt C"), "record.color");
         inputMap.put(KeyStroke.getKeyStroke("alt L"), "lock.position");
@@ -196,15 +113,101 @@ public class ColorPickerPanel extends EasyPanel {
         actionMap.put("lock.position", lockPositionAction);
         actionMap.put("unlock.position", unlockPositionAction);
 
-        // 鼠标监听
         mouseListener();
     }
 
-    /**
-     * 按键动作
-     * alt+c 触发此动作，用来记录颜色值
-     */
-    public class RecordColorAction extends AbstractAction {
+    private void initFirstColumn() {
+        // 左侧的颜色框和显示的坐标，颜色值
+        // 添加 panel 组件用来做颜色框
+        colorPanel = new JPanel();
+        colorPanel.setBounds(LEFT_PADDING, TOP_PADDING, DEFAULT_WIDTH, DEFAULT_HEIGHT_BIG);
+        add(colorPanel);
+
+        coordinateLabel = new JLabel();
+        coordinateLabel.setBounds(LEFT_PADDING, TOP_PADDING + DEFAULT_HEIGHT_BIG + Constants.DEFAULT_Y_BORDER, DEFAULT_WIDTH, DEFAULT_HEIGHT_SMALL);
+        add(coordinateLabel);
+
+        colorLabel = new JLabel();
+        colorLabel.setBounds(LEFT_PADDING, TOP_PADDING + DEFAULT_HEIGHT_BIG + DEFAULT_HEIGHT_SMALL + Constants.DEFAULT_Y_BORDER * 2, DEFAULT_WIDTH, DEFAULT_HEIGHT_SMALL);
+        add(colorLabel);
+
+        JComboBox<String> colorModeCombo = new JComboBox<>();
+        for (ColorMode t : ColorMode.values()) {
+            colorModeCombo.addItem(t.toString());
+        }
+
+        colorModeCombo.addActionListener(event -> {
+            currentColorMode = ColorMode.valueOf((String) colorModeCombo.getSelectedItem());
+        });
+        colorModeCombo.setBounds(LEFT_PADDING, TOP_PADDING + DEFAULT_HEIGHT_BIG + DEFAULT_HEIGHT_SMALL * 2 + Constants.DEFAULT_Y_BORDER * 3, DEFAULT_WIDTH, DEFAULT_HEIGHT_SMALL);
+        add(colorModeCombo);
+    }
+
+    private void initSecondColumn() {
+        crossHorizontal = new Line2D.Double(
+                (double) LEFT_PADDING + DEFAULT_WIDTH + Constants.DEFAULT_X_BORDER,
+                (double) TOP_PADDING + DEFAULT_HEIGHT_BIG / 2.0,
+                (double) LEFT_PADDING + DEFAULT_WIDTH * 2 + Constants.DEFAULT_X_BORDER,
+                (double) TOP_PADDING + DEFAULT_HEIGHT_BIG / 2.0);
+        crossVertical = new Line2D.Double(
+                (double) LEFT_PADDING + DEFAULT_WIDTH + Constants.DEFAULT_X_BORDER + DEFAULT_WIDTH / 2.0,
+                TOP_PADDING,
+                (double) LEFT_PADDING + DEFAULT_WIDTH + Constants.DEFAULT_X_BORDER + DEFAULT_WIDTH / 2.0,
+                (double) TOP_PADDING + DEFAULT_HEIGHT_BIG);
+
+        colorCopyTextField = new JTextField();
+        colorCopyTextField.setEditable(false);
+        colorCopyTextField.setBounds(LEFT_PADDING + DEFAULT_WIDTH + Constants.DEFAULT_X_BORDER, TOP_PADDING + DEFAULT_HEIGHT_BIG + Constants.DEFAULT_Y_BORDER, DEFAULT_WIDTH, DEFAULT_HEIGHT_SMALL);
+        add(colorCopyTextField);
+
+        colorCopyButton = new JButton("Copy");
+        colorCopyButton.addActionListener(event -> {
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            Transferable trans = new StringSelection(getColorText(currentColor));
+            clipboard.setContents(trans, null);
+        });
+        colorCopyButton.setBounds(LEFT_PADDING + DEFAULT_WIDTH + Constants.DEFAULT_X_BORDER, TOP_PADDING + DEFAULT_HEIGHT_BIG + Constants.DEFAULT_Y_BORDER * 2 + DEFAULT_HEIGHT_SMALL, DEFAULT_WIDTH, DEFAULT_HEIGHT_SMALL);
+        add(colorCopyButton);
+    }
+
+    private void initThirdColumn() {
+        for (int i = 0; i < colorRecordMax; i++) {
+            colorRecordValue[i] = new JLabel();
+            colorRecordValue[i].setOpaque(true); // 背景不透明
+            colorRecordValue[i].setBounds(LEFT_PADDING + DEFAULT_WIDTH * 2 + Constants.DEFAULT_X_BORDER * 2,
+                    TOP_PADDING + i * DEFAULT_HEIGHT_SMALL, DEFAULT_WIDTH, DEFAULT_HEIGHT_SMALL);
+            add(colorRecordValue[i]);
+        }
+        JComboBox<String> magnificationModeCombo = new JComboBox<>();
+        magnificationModeCombo.addItem("Zoom: 100%");
+        magnificationModeCombo.addItem("Zoom: 200%");
+        magnificationModeCombo.addItem("Zoom: 300%");
+        magnificationModeCombo.addItem("Zoom: 400%");
+        magnificationModeCombo.addItem("Zoom: 500%");
+        magnificationModeCombo.addItem("Zoom: 1200%");
+        magnificationModeCombo.addItem("Zoom: 2800%");
+        magnificationModeCombo.setSelectedItem("Zoom: 200%");
+        magnificationModeCombo.addActionListener(event -> {
+            String selectedItem = (String) magnificationModeCombo.getSelectedItem();
+            String tmp = StringUtils.substringBetween(selectedItem, "Zoom: ", "%");
+            zoomFactor = Integer.valueOf(tmp) / 100;
+            logger.info("zoomFactor: " + zoomFactor);
+        });
+        magnificationModeCombo.setBounds(LEFT_PADDING + Constants.DEFAULT_X_BORDER * 2 + DEFAULT_WIDTH * 2, TOP_PADDING + DEFAULT_HEIGHT_BIG + Constants.DEFAULT_Y_BORDER, DEFAULT_WIDTH, DEFAULT_HEIGHT_SMALL);
+        add(magnificationModeCombo);
+    }
+
+    private void initFourthColumn() {
+        JTextArea textArea = new JTextArea();
+        textArea.setBackground(Color.PINK);
+        textArea.setBounds(LEFT_PADDING + DEFAULT_WIDTH * 3 + Constants.DEFAULT_X_BORDER * 3, TOP_PADDING, TEXT_AREA_WIDTH, DEFAULT_HEIGHT_BIG);
+        textArea.append("记录颜色: ALT+C" + System.getProperty("line.separator"));
+        textArea.append("锁定区域: ALT+L" + System.getProperty("line.separator"));
+        textArea.append("取消锁定区域: ALT+U" + System.getProperty("line.separator"));
+        add(textArea);
+    }
+
+    class RecordColorAction extends AbstractAction {
         private static final long serialVersionUID = 1L;
 
         @Override
@@ -213,18 +216,17 @@ public class ColorPickerPanel extends EasyPanel {
                 logger.error("robot is null");
                 return;
             }
-            // 记录当前的颜色
-            Color c = robot.getPixelColor(mousePoint.x, mousePoint.y);
-            currentColor = c;
+            Color color = robot.getPixelColor(mousePoint.x, mousePoint.y);
+            currentColor = color;
 
             // 头进尾出，就可以满足绘制到顶部的记录是最新的
-            colorQueue.offerFirst(c);
+            colorQueue.offerFirst(color);
             if (colorQueue.size() > colorRecordMax) {
                 colorQueue.pollLast(); // 删除尾
             }
 
             // 显示颜色值到文本框
-            showField.setText(getColorText(c));
+            colorCopyTextField.setText(getColorText(color));
             repaint();
         }
     }
@@ -254,10 +256,7 @@ public class ColorPickerPanel extends EasyPanel {
         return new Dimension(WIDTH, HEIGHT);
     }
 
-    /**
-     * 鼠标监听，利用 timer
-     */
-    public void mouseListener() {
+    private void mouseListener() {
         try {
             robot = new Robot();
         } catch (final AWTException e) {
@@ -298,35 +297,31 @@ public class ColorPickerPanel extends EasyPanel {
         final Color pixel = robot.getPixelColor(mousePoint.x, mousePoint.y);
         colorPanel.setBackground(pixel);
 
-        coordsLabel.setText(String.format("[%d, %d]", mousePoint.x, mousePoint.y));
+        coordinateLabel.setText(String.format("[%d, %d]", mousePoint.x, mousePoint.y));
         colorLabel.setText(getColorText(pixel));
 
         if (!isLocked) {
-            // 获取区域
             getMouseArea();
         }
     }
 
-    /**
-     * 根据当前颜色模式显示颜色值
-     */
-    private String getColorText(final Color c) {
-        if (c == null) {
+    private String getColorText(final Color color) {
+        if (color == null) {
             return "";
         }
         String s = "";
         switch (currentColorMode) {
             case RGB:
-                s = String.format("%d, %d, %d", c.getRed(), c.getGreen(), c.getBlue());
+                s = String.format("%d, %d, %d", color.getRed(), color.getGreen(), color.getBlue());
                 break;
             case HTML:
-                s = String.format("#%02x%02x%02x", c.getRed(), c.getGreen(), c.getBlue());
+                s = String.format("#%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue());
                 break;
             case HEX:
-                s = String.format("0x%02X%02X%02X", c.getBlue(), c.getGreen(), c.getRed());
+                s = String.format("0x%02X%02X%02X", color.getBlue(), color.getGreen(), color.getRed());
                 break;
             case HSB:
-                hsbArr = Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(), null);
+                hsbArr = Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), null);
                 s = String.format("%3.0f%% %3.0f%% %3.0f%%", hsbArr[0] * 100, hsbArr[1] * 100, hsbArr[2] * 100);
                 break;
             default:
@@ -336,15 +331,13 @@ public class ColorPickerPanel extends EasyPanel {
 
     }
 
-    /**
-     * 获取鼠标周围区域图片
-     */
     protected void getMouseArea() {
 
         final int x = mousePoint.x;
         final int y = mousePoint.y;
 
-        final Rectangle r = new Rectangle(x - sideLength / 2, y - sideLength / 2, sideLength, sideLength);
+        int length = 100 / zoomFactor;
+        final Rectangle r = new Rectangle(x - length / 2, y - length / 2, length, length);
         if (robot != null) {
             areaImage = robot.createScreenCapture(r);
         }
@@ -361,7 +354,7 @@ public class ColorPickerPanel extends EasyPanel {
         // 中间放大镜
         final Graphics2D g2 = (Graphics2D) g;
         //g2.drawImage(areaImage,10,300,null); // 原大小
-        g2.drawImage(areaImage, LEFT_PADDING + DEFAULT_WIDTH + Constants.DEFAULT_X_BORDER, TOP_PADDING, DEFAULT_WIDTH, ZOOM_RECT_HEIGHT, null);
+        g2.drawImage(areaImage, LEFT_PADDING + DEFAULT_WIDTH + Constants.DEFAULT_X_BORDER, TOP_PADDING, DEFAULT_WIDTH, DEFAULT_HEIGHT_BIG, null);
         // 放大镜的十字线
         g2.setPaint(Color.RED);
         g2.draw(crossHorizontal);
@@ -374,7 +367,7 @@ public class ColorPickerPanel extends EasyPanel {
         paintBorder(g2);
 
         // 颜色值重新赋值，因为可能有模式的改变
-        showField.setText(getColorText(currentColor));
+        colorCopyTextField.setText(getColorText(currentColor));
 
     }
 
@@ -397,23 +390,22 @@ public class ColorPickerPanel extends EasyPanel {
     private void paintBorder(Graphics2D g2) {
         g2.setPaint(Color.BLACK);
         colorRect.setFrameFromDiagonal(
-                (double)LEFT_PADDING - 1,
-                (double)TOP_PADDING - 1,
-                (double)LEFT_PADDING + DEFAULT_WIDTH,
-                (double)TOP_PADDING + COLOR_RECT_HEIGHT);
+                (double) LEFT_PADDING - 1,
+                (double) TOP_PADDING - 1,
+                (double) LEFT_PADDING + DEFAULT_WIDTH,
+                (double) TOP_PADDING + DEFAULT_HEIGHT_BIG);
         zoomRect.setFrameFromDiagonal(
-                (double)LEFT_PADDING + DEFAULT_WIDTH + Constants.DEFAULT_X_BORDER - 1,
-                (double)TOP_PADDING - 1,
-                (double)LEFT_PADDING + DEFAULT_WIDTH * 2 + Constants.DEFAULT_X_BORDER,
-                (double)TOP_PADDING + ZOOM_RECT_HEIGHT);
+                (double) LEFT_PADDING + DEFAULT_WIDTH + Constants.DEFAULT_X_BORDER - 1,
+                (double) TOP_PADDING - 1,
+                (double) LEFT_PADDING + DEFAULT_WIDTH * 2 + Constants.DEFAULT_X_BORDER,
+                (double) TOP_PADDING + DEFAULT_HEIGHT_BIG);
         recordRect.setFrameFromDiagonal(
-                (double)LEFT_PADDING + DEFAULT_WIDTH * 2 + Constants.DEFAULT_X_BORDER * 2 - 1,
-                (double)TOP_PADDING - 1,
-                (double)LEFT_PADDING + DEFAULT_WIDTH * 3 + Constants.DEFAULT_X_BORDER * 2,
-                (double)TOP_PADDING + RECORD_RECT_HEIGHT);
+                (double) LEFT_PADDING + DEFAULT_WIDTH * 2 + Constants.DEFAULT_X_BORDER * 2 - 1,
+                (double) TOP_PADDING - 1,
+                (double) LEFT_PADDING + DEFAULT_WIDTH * 3 + Constants.DEFAULT_X_BORDER * 2,
+                (double) TOP_PADDING + DEFAULT_HEIGHT_BIG);
         g2.draw(colorRect);
         g2.draw(zoomRect);
         g2.draw(recordRect);
     }
-
 }
