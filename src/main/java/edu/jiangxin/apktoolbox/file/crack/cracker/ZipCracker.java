fe77b9cd3ca6de0118fbe5aff12c9a686209d18b
Aloys <jiangxinnju@163.com>
2022-05-01 12:22:37 +0800

    add faster zip cracker

---
+++ src/main/java/edu/jiangxin/apktoolbox/file/crack/cracker/ZipCracker.java
@@ -0,0 +1,59 @@
+package edu.jiangxin.apktoolbox.file.crack.cracker;
+
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+public final class ZipCracker extends FileCracker {
+    private static final boolean DEBUG = false;
+
+    public ZipCracker() {
+        super();
+    }
+
+    @Override
+    public String[] getFileExtensions() {
+        return new String[]{"zip"};
+    }
+
+    @Override
+    public String getFileDescription() {
+        return "*.zip";
+    }
+
+    @Override
+    public String getDescription() {
+        return "ZIP Cracker";
+    }
+
+    @Override
+    public boolean prepareCracker() {
+        return true;
+    }
+
+    @Override
+    public boolean checkPwd(String pwd) {
+        boolean result = false;
+        try {
+            ZipFile zFile = new ZipFile(file);
+            zFile.setFileNameCharset(StandardCharsets.UTF_8.name());
+            String dest = file.getAbsolutePath().replace(".zip", "Tmp");
+            File destDir = new File(dest);
+            if (destDir.exists()) {
+                destDir.delete();
+            }
+            if (!destDir.exists()) {
+                destDir.mkdirs();
+            }
+            if (zFile.isEncrypted()) {
+                zFile.setPassword(pwd.toCharArray());
+            }
+            zFile.extractAll(dest);
+            result = true;
+        } catch (ZipException e) {
+        }
+        return result;
+    }
+}
