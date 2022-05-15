bfcdd9d566960e3eb3747718f5e5e53d6233a49d
Aloys <jiangxinnju@163.com>
2022-05-15 15:08:19 +0800

    update Package script to create Portable app with JRE

--- Package.ps1
+++ Package.ps1
@@ -2,22 +2,49 @@ $CURRENT_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $TARGET_DIR = "$CURRENT_DIR/target"
 $RELEASE_JAR_FILE_Filter = 'APKToolBoxGUI-*.jar'
 $RELEASE_JAR_FILE = Get-ChildItem -Path "$TARGET_DIR" -Filter $RELEASE_JAR_FILE_Filter
-
-$RELEASE_DIR = "$TARGET_DIR/release"
-if (Test-Path $RELEASE_DIR) {
-    Remove-Item $RELEASE_DIR -recurse
-}
-If (!(Test-Path $RELEASE_DIR)) {
-    New-Item -ItemType Directory -Force -Path $RELEASE_DIR
-}
-
-Copy-Item $RELEASE_JAR_FILE.FullName "$RELEASE_DIR/APKToolBoxGUI.jar"
-
 $VERSION = $RELEASE_JAR_FILE.BaseName.Replace("APKToolBoxGUI-", "").Replace(".jar", "")
+$TMP_DIR = "$TARGET_DIR/release"
 
-Copy-Item "$CURRENT_DIR/ApkToolBoxGUI.bat" $RELEASE_DIR
-Copy-Item "$CURRENT_DIR/ApkToolBoxGUI.sh" $RELEASE_DIR
-Copy-Item "$CURRENT_DIR/apktoolboxgui.properties" $RELEASE_DIR
-Copy-Item "$CURRENT_DIR/tools" $RELEASE_DIR -Recurse
+# Builed $TARGET_DIR/APKToolBoxGUI-$VERSION-without-JRE.zip
+if (Test-Path $TMP_DIR) {
+    Remove-Item $TMP_DIR -Recurse -Force
+}
+If (!(Test-Path $TMP_DIR)) {
+    New-Item -ItemType Directory -Force -Path $TMP_DIR
+}
 
-Compress-Archive -Path "$RELEASE_DIR\*" -DestinationPath "$TARGET_DIR/APKToolBoxGUI-$VERSION.zip" -Force
+Copy-Item $RELEASE_JAR_FILE.FullName "$TMP_DIR/APKToolBoxGUI.jar"
+
+Copy-Item "$CURRENT_DIR/ApkToolBoxGUI.bat" $TMP_DIR
+Copy-Item "$CURRENT_DIR/ApkToolBoxGUI.sh" $TMP_DIR
+Copy-Item "$CURRENT_DIR/apktoolboxgui.properties" $TMP_DIR
+
+Compress-Archive -Path "$TMP_DIR\*" -DestinationPath "$TARGET_DIR/APKToolBoxGUI-$VERSION-without-JRE.zip" -Force
+
+
+# Builed $TARGET_DIR/APKToolBoxGUI-$VERSION-with-JRE.zip
+if (Test-Path $TMP_DIR) {
+    Remove-Item $TMP_DIR -Recurse -Force
+}
+If (!(Test-Path $TMP_DIR)) {
+    New-Item -ItemType Directory -Force -Path $TMP_DIR
+}
+if (Test-Path "$TARGET_DIR/ApkToolBoxGUI-$VERSION-with-JRE") {
+    Remove-Item "$TARGET_DIR/ApkToolBoxGUI-$VERSION-with-JRE" -recurse
+}
+
+Copy-Item $RELEASE_JAR_FILE.FullName "$TMP_DIR/APKToolBoxGUI.jar"
+
+jpackage --input $TMP_DIR --type app-image --name "ApkToolBoxGUI-$VERSION-with-JRE" --main-jar 'APKToolBoxGUI.jar' --dest "$TARGET_DIR" --verbose
+
+Copy-Item "$CURRENT_DIR/apktoolboxgui.properties" "$TARGET_DIR/ApkToolBoxGUI-$VERSION-with-JRE"
+
+Compress-Archive -Path "$TARGET_DIR/ApkToolBoxGUI-$VERSION-with-JRE\*" -DestinationPath "$TARGET_DIR/APKToolBoxGUI-$VERSION-with-JRE.zip" -Force
+
+if (Test-Path $TMP_DIR) {
+    Remove-Item $TMP_DIR -Recurse -Force
+}
+
+if (Test-Path "$TARGET_DIR/ApkToolBoxGUI-$VERSION-with-JRE") {
+    Remove-Item "$TARGET_DIR/ApkToolBoxGUI-$VERSION-with-JRE" -Recurse -Force
+}
--- Package.sh
+++ Package.sh
@@ -3,30 +3,65 @@ set -x
 CURRENT_DIR=`pwd`
 TARGET_DIR=$CURRENT_DIR/target
 RELEASE_JAR_FILE=`ls -al target/APKToolBoxGUI-*.jar | awk '{print $9}'`
-
-RELEASE_DIR="$TARGET_DIR/release"
-
-if [ -d $RELEASE_DIR ];
-then
-    rm -rf $RELEASE_DIR
-fi
-
-if [ ! -d $RELEASE_DIR ];
-then
-    mkdir -p $RELEASE_DIR
-fi
-
-cp $RELEASE_JAR_FILE $RELEASE_DIR/APKToolBoxGUI.jar
-
 VERSION=${RELEASE_JAR_FILE#*APKToolBoxGUI-}
 VERSION=${VERSION%*.jar}
+TMP_DIR="$TARGET_DIR/release"
 
-cp $CURRENT_DIR/ApkToolBoxGUI.bat $RELEASE_DIR
-cp $CURRENT_DIR/ApkToolBoxGUI.sh $RELEASE_DIR
-cp $CURRENT_DIR/apktoolboxgui.properties $RELEASE_DIR
-cp -r $CURRENT_DIR/tools $RELEASE_DIR
+# Builed $TARGET_DIR/APKToolBoxGUI-$VERSION-without-JRE.zip
+if [ -d $TMP_DIR ];
+then
+    rm -rf $TMP_DIR
+fi
 
-cd $RELEASE_DIR
-zip -r APKToolBoxGUI-$VERSION.zip *
+if [ ! -d $TMP_DIR ];
+then
+    mkdir -p $TMP_DIR
+fi
+
+cp $RELEASE_JAR_FILE $TMP_DIR/APKToolBoxGUI.jar
+
+cp $CURRENT_DIR/ApkToolBoxGUI.bat $TMP_DIR
+cp $CURRENT_DIR/ApkToolBoxGUI.sh $TMP_DIR
+cp $CURRENT_DIR/apktoolboxgui.properties $TMP_DIR
+
+cd $TMP_DIR
+zip -r APKToolBoxGUI-$VERSION-without-JRE.zip *
 cd $CURRENT_DIR
-mv $RELEASE_DIR/APKToolBoxGUI-$VERSION.zip $TARGET_DIR
+mv $TMP_DIR/APKToolBoxGUI-$-without-JRE.zip $TARGET_DIR
+
+
+# Builed $TARGET_DIR/APKToolBoxGUI-$VERSION-with-JRE.zip
+if [ -d $TMP_DIR ];
+then
+    rm -rf $TMP_DIR
+fi
+
+if [ ! -d $TMP_DIR ];
+then
+    mkdir -p $TMP_DIR
+fi
+
+if [ -d $TMP_DIR ];
+then
+    rm -rf $TMP_DIR
+fi
+
+cp $RELEASE_JAR_FILE $TMP_DIR/APKToolBoxGUI.jar
+
+jpackage --input $TMP_DIR --type app-image --name ApkToolBoxGUI-$VERSION-with-JRE --main-jar APKToolBoxGUI.jar --dest $TARGET_DIR --verbose
+
+cp $CURRENT_DIR/apktoolboxgui.properties $TARGET_DIR/ApkToolBoxGUI-$VERSION-with-JRE
+
+cd $TARGET_DIR
+zip -r $TARGET_DIR/APKToolBoxGUI-$VERSION-with-JRE.zip *
+cd $CURRENT_DIR
+
+if [ -d $TMP_DIR ];
+then
+    rm -rf $TMP_DIR
+fi
+
+if [ -d $TARGET_DIR/ApkToolBoxGUI-$VERSION-with-JRE ];
+then
+    rm -rf $TARGET_DIR/ApkToolBoxGUI-$VERSION-with-JRE
+fi
